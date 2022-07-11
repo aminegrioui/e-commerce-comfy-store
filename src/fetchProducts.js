9daeea298ccdea62145d811eed4f75aa581c1551
amine <amine23grioui@gmail,com>
2022-07-11 15:00:52 +0200

    fetch products

--- src/fetchProducts.js
+++ src/fetchProducts.js
@@ -1,5 +1,16 @@
 import { allProductsUrl } from './utils.js';
 
-const fetchProducts = async () => {};
+const fetchProducts = async () => {
+
+    
+        const response=await fetch(allProductsUrl)
+        .catch((error)=> console.log(error));
+        if(response){
+            return  response.json();
+        }
+        return response;
+        
+    
+};
 
 export default fetchProducts;
