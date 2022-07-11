bc771c89f7654d9459ddae692732a47a50648d6c
amine <amine23grioui@gmail,com>
2022-07-11 17:00:58 +0200

    laoding page

--- src/pages/products.js
+++ src/pages/products.js
@@ -12,4 +12,10 @@ import setupPrice from '../filters/price.js';
 import { store } from '../store.js';
 import display from '../displayProducts.js';
 import { getElement } from '../utils.js';
+
+const laoding=getElement('.page-loading')
+
 display(store, getElement('.products-container'))
+
+laoding.style.display='none'
+
