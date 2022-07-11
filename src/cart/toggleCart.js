0eaacfa31fd34c9a169bf1e83826477c96ae7e79
amine <amine23grioui@gmail,com>
2022-07-11 13:01:55 +0200

    toggle cart

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,17 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const cartOverlay=getElement('.cart-overlay')
+const closCartBtn=getElement('.cart-close')
+const toggleCartBtn=getElement('.toggle-cart')
+
+toggleCartBtn.addEventListener('click',()=>{
+    cartOverlay.classList.add('show')
+})
+
+closCartBtn.addEventListener('click',()=>{
+    cartOverlay.classList.remove('show')
+})
+
+export const openCart = () => {
+    cartOverlay.classList.add('show')
+};
