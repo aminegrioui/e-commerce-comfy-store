e00e4b8fbaa7874f638f16957cd3e504cd6cc1a9
amine <amine23grioui@gmail,com>
2022-07-12 16:12:33 +0200

    search filter

--- src/filters/search.js
+++ src/filters/search.js
@@ -1,5 +1,31 @@
 import { getElement } from '../utils.js';
 import display from '../displayProducts.js';
-const setupSearch = () => {};
+
+const form=getElement('.input-form')
+const input=getElement('.search-input')
+
+console.log(input.value)
+const setupSearch = (store) => {
+    form.addEventListener('keyup',(e)=>{
+        const value=input.value
+       
+        const element=getElement('.products-container');
+        if(value){
+             const newStore=store.filter(product=> product.name.includes(value));
+            if(newStore.length>0){
+                display(newStore,element)
+            }
+            else{
+                element.innerHTML=`<h3 class="filter-error">
+                    sorry, no products matched your search
+                </h3>`
+            }
+        }
+       
+      
+
+        
+    })
+};
 
 export default setupSearch;
